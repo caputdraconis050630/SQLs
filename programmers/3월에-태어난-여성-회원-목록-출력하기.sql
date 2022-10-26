46c33d52c5f1841431b53817511b75657b867a6b
Guntak Kim <caputdraconis@kakao.com>
2022-10-26 22:18:17 +0900

    221026 SQL

---
+++ programmers/3월에-태어난-여성-회원-목록-출력하기.sql
@@ -0,0 +1,11 @@
+/*
+	3월에 태어난 여성 회원 목록 출력하기
+	https://school.programmers.co.kr/learn/courses/30/lessons/131120
+*/
+
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE TLNO IS NOT NULL
+AND MONTH(DATE_OF_BIRTH) = 3
+AND GENDER = 'W'
+ORDER BY MEMBER_ID ASC;
---
+++ programmers/최댓값-구하기.sql
@@ -0,0 +1,7 @@
+/*
+	최댓값 구하기
+	https://school.programmers.co.kr/learn/courses/30/lessons/59415
+*/
+
+SELECT MAX(DATETIME)
+FROM ANIMAL_INS
